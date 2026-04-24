410c741ae9344d8f21cbfd3bb41cb809cf61034b
Fexx24 <felhuf@gmail.com>
2026-04-24 15:49:57 +0200

    initial

--- src/components/Header/Header.vue
+++ src/components/Header/Header.vue
@@ -13,7 +13,7 @@
       <ul class="nav-menu" :class="{ active: mobileMenuOpen }">
         <li>
           <a href="#home" class="home" aria-current="page">
-            Home
+            Home1
           </a>
         </li>
         <li>
